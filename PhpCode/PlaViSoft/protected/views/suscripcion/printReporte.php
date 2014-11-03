4b6ada54af82646033dfea33e2a23eff3eea2cb0
Diego Rodriguez <diego@qwavee.com>
2014-11-03 12:34:42 -0300

    Reporte Suscripciones - Encabezado

--- PhpCode/PlaViSoft/protected/views/suscripcion/printReporte.php
+++ PhpCode/PlaViSoft/protected/views/suscripcion/printReporte.php
@@ -1,6 +1,6 @@
 <style type="text/css">
 <!--
-    table.page_header {width: 100%; border: none; background-color: #DDDDFF; border-bottom: solid 1mm #AAAADD; padding: 2mm }
+    table.page_header {width: 100%; border: none; border-bottom: solid 1mm #000000; padding: 2mm }
     table.page_footer {width: 100%; border: none; background-color: #DDDDFF; border-top: solid 1mm #AAAADD; padding: 2mm}
 
     div.niveau
@@ -14,7 +14,7 @@
             <table class="page_header">
                 <tr>
                     <td style="width: 100%; text-align: left">
-                        CVI Constructora
+                        CVI Constructora - Reporte de Suscripción
                     </td>
                 </tr>
             </table>
@@ -22,14 +22,34 @@
 	<page_footer>
             <table class="page_footer">
                 <tr>
-                    <td style="width: 100%; text-align: right">
+                    <td style="width: 50%; text-align: left">
                         Página: [[page_cu]]/[[page_nb]]
+                    </td>
+                    <td style="width: 50%; text-align: right">
+                        Fecha de Reporte: <?php echo date('d/m/Y');?>
                     </td>
                 </tr>
             </table>
 	</page_footer>
     
     
+        <table width="100%">
+            <tr>
+                <td>
+                    <?php echo CHtml::image(Yii::app()->request->getBaseUrl(true).'/img/cvi150.jpg'); ?>
+                </td>
+                <td>
+                    <p>
+                        <h2><?php echo $suscripcion->persona->nombreCompleto; ?></h2>
+                        <?php echo "Suscripción: <b>".$suscripcion->nombreStr."</b>"; ?>
+                        <?php echo "<br>Financiacion: <b>".$suscripcion->financiacion->tipoVivienda->Descripcion."</b>"; ?>
+                        <?php echo "<br>Fecha de Reporte: <b>".date('d/m/Y')."</b>"; ?>
+                        
+                    </p>
+                </td>
+            </tr>
+        </table>
+        <br><br>
         <table width=700>
             <thead>
                 <tr>
